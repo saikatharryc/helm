294cc06a6ba67ece081c983f81d92c432d257d9e
Saikat Chakrabortty <saikatchakrabortty2@gmail.com>
2020-04-19 13:41:50 +0530

    ci: added ci in the process (#1)

---
+++ test/e2e-kind.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly CT_VERSION=v3.0.0-rc.1
+readonly KIND_VERSION=v0.7.0
+readonly CLUSTER_NAME=chart-testing
+readonly K8S_VERSION=v1.17.0
+
+run_ct_container() {
+    echo 'Running ct container...'
+    docker run --rm --interactive --detach --network host --name ct \
+        -e KUBECONFIG="/root/.kube/config" \
+        --volume "$(pwd)/test/ct.yaml:/etc/ct/ct.yaml" \
+        --volume "$(pwd):/workdir" \
+        --workdir /workdir \
+        "quay.io/helmpack/chart-testing:$CT_VERSION" \
+        cat
+    echo
+}
+
+cleanup() {
+    echo 'Removing ct container...'
+    docker kill ct > /dev/null 2>&1
+
+    echo 'Done!'
+}
+
+docker_exec() {
+    docker exec --interactive ct "$@"
+}
+
+create_kind_cluster() {
+    echo 'Installing kind...'
+
+    curl -sSLo kind "https://github.com/kubernetes-sigs/kind/releases/download/$KIND_VERSION/kind-linux-amd64"
+    chmod +x kind
+    sudo mv kind /usr/local/bin/kind
+
+    kind create cluster --name "$CLUSTER_NAME" --config test/kind-config.yaml --image "kindest/node:$K8S_VERSION" --kubeconfig config --wait 60s
+
+    echo 'Copying kubeconfig to container...'
+    # echo  "$KUBECONFIG"
+    # cat config
+    docker_exec mkdir /root/.kube
+    docker_exec touch  /root/.kube/config
+    sudo docker cp config ct:/root/.kube/config
+    docker_exec touch /root/.kube/config
+    docker_exec kubectl cluster-info
+    echo
+
+    docker_exec kubectl get nodes
+    echo
+
+    echo 'Cluster ready!'
+    echo
+}
+
+install_charts() {
+    docker_exec ct install
+    echo
+}
+
+main() {
+    run_ct_container
+    trap cleanup EXIT
+
+    create_kind_cluster
+    install_charts
+}
+
+main
